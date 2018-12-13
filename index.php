f5d92282869b8e26410168dba6a92fc0527c6e05
shikharmakker <shikharmakker@gmail.com>
2018-12-13 12:37:04 +0530

    add index

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Girnar House</title>
+</head>
+<body>
+
+<!--  Header Start -->
+
+
+<!--  Header End -->
+
+
+<!-- acivity area start -->
+
+
+
+
+
+<!-- actvity area end -->
+
+
+
+<!--  Footer Start -->
+
+
+<!--  Footer End -->
+
+</body>
+</html>
